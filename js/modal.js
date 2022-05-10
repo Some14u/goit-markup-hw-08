821c8807693f6139b65d59191e11c53db7176765
Some14u <some1@email.ua>
2022-05-10 14:26:33 +0300

    Свёрстана базовая резиновая мобильная версия

--- js/modal.js
+++ js/modal.js
@@ -3,18 +3,34 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    modals: document.querySelectorAll("[data-modal]")
+    modals: document.querySelectorAll("[data-modal]"),
+    body: document.querySelector("body"),
+    openMobileMenuBtn:  document.querySelector("[data-mobile-menu-open]"),
+    mobileMenu: document.querySelector("[data-mobile-menu]")
   }
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  refs.modal.addEventListener("click", (event) => {
+  if (refs.openModalBtn) refs.openModalBtn.addEventListener("click", toggleModal);
+  if (refs.closeModalBtn) refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  if (refs.openMobileMenuBtn) refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+
+
+  if (refs.modal) refs.modal.addEventListener("click", (event) => {
     if (event.target===refs.modal) toggleModal();
   })
 
   function toggleModal(event) {
+    refs.body.classList.toggle("body--locked");
     refs.modals.forEach(element => {
       element.classList.toggle(element.classList[0] + "--hidden");
     });
   }
+
+  function toggleMobileMenu(event) {
+    window.scrollTo(0, 0);
+    refs.body.classList.toggle("body--locked");
+    refs.mobileMenu.classList.toggle(refs.mobileMenu.classList[0] + "--hidden");
+    refs.openMobileMenuBtn.classList.toggle(refs.openMobileMenuBtn.classList[0] + "--opened");
+  }
+
 })();
